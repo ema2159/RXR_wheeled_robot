e0ce24bd965b27dd2c54ad8ccdef018aa79b5417
Emmanuel Bustos <ema2159@gmail.com>
2020-11-26 11:28:40 +0200

    Add: Testing socket client

---
+++ socket_to_serial/socket_client.py
@@ -0,0 +1,14 @@
+import socket
+import time
+import random
+
+HOST = '10.138.226.50'  # The server's hostname or IP address
+PORT = 65432        # The port used by the server
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    while True:
+        data = str.encode("["+str(random.randint(0, 100))+","+str(random.randint(0, 100))+"]")
+        s.send(data)
+        time.sleep(0.1)
+        
