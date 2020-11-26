cd5217e70435d8ace97cfa426129d8603bdf0dd9
Emmanuel Bustos <ema2159@gmail.com>
2020-11-26 14:47:30 +0200

    Mod: Remove unnecessary square brackets

--- socket_to_serial/socket_client.py
+++ socket_to_serial/socket_client.py
@@ -8,7 +8,7 @@ PORT = 65432        # The port used by the server
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     while True:
-        data = str.encode("["+str(random.randint(0, 100))+","+str(random.randint(0, 100))+"]")
+        data = str.encode(str(random.randint(0, 100))+","+str(random.randint(0, 100)))
         s.send(data)
         time.sleep(0.1)
         
