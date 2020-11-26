bd7ffb2ad53417b44ca189c4a6f34aa2bdfff6b0
Emmanuel Bustos <ema2159@gmail.com>
2020-11-26 15:10:45 +0200

    Clean: Formatted files

--- socket_to_serial/socket_client.py
+++ socket_to_serial/socket_client.py
@@ -2,13 +2,19 @@ import socket
 import time
 import random
 
-HOST = '10.138.226.50'  # The server's hostname or IP address
-PORT = 65432        # The port used by the server
+HOST = "10.138.226.50"  # The server's hostname or IP address
+PORT = 65432  # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     while True:
-        data = str.encode(str(random.randint(0, 100))+","+str(random.randint(0, 100)))
+        data = str.encode(
+            str(random.randint(0, 100))
+            + ","
+            + str(random.randint(0, 100))
+            + ","
+            + str(random.randint(0, 100))
+            + "\n"
+        )
         s.send(data)
         time.sleep(0.1)
-        
--- socket_to_serial/socket_server.py
+++ socket_to_serial/socket_server.py
@@ -2,8 +2,8 @@ import socket
 import time
 import random
 
-HOST = '10.138.226.50'  # Standard loopback interface address (localhost)
-PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+HOST = "10.138.226.50"  # Standard loopback interface address (localhost)
+PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Allow reconect when the server falls
@@ -13,7 +13,7 @@ s.bind((HOST, PORT))
 s.listen()
 conn, addr = s.accept()
 with conn:
-    print('Connected by', addr)
+    print("Connected by", addr)
     while True:
         data = conn.recv(1024)
         if not data:
